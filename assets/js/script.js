2713fd80c6000e8787c6b4bfca9f68889d7eb531
HadellHadell <hermanefraim00@gmail.com>
2022-09-21 07:43:58 +0000

    write script code in JV and test the connection

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const startButton = document.getElementById('control-start-btn')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Game started')
+}
+function setNextQuestion() {
+
+}
+function selectAnswer() {
+
+}
