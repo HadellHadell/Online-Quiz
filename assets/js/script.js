631774ecb719c7aae3a18f029a281969b96e521c
HadellHadell <hermanefraim00@gmail.com>
2022-09-21 08:00:23 +0000

    set the first question in the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 const startButton = document.getElementById('control-start-btn')
+const questionContainerElement = document.getElementById('question-container')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Game started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 function setNextQuestion() {
 
@@ -11,3 +15,15 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'What year did the composer Johann Sebastian Bach die?',
+        answers: [
+            { text: '1750', correct: true},
+            { text: '1758', correct: false},
+            { text: '1748', correct: false},
+            { text: '1754', correct: false},
+        ]
+    }
+]
