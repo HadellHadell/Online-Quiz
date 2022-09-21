2769f0a07921ed836a2dce702d08ff0dbd8262ea
HadellHadell <hermanefraim00@gmail.com>
2022-09-21 09:09:12 +0000

    Show the answer alternative buttons with each question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,35 @@
 const startButton = document.getElementById('control-start-btn')
 const questionContainerElement = document.getElementById('question-container')
+let randomQuestion, currentQuestionIndex
+const questionElement = document.getElementById('question')
+const answersButtonElement = document.getElementById('answer-button')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Game started')
     startButton.classList.add('hide')
+    randomQuestion = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 function setNextQuestion() {
-
+    showQuestion(randomQuestion[currentQuestionIndex])
 }
+function showQuestion(_questions) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
+  }
 function selectAnswer() {
 
 }
